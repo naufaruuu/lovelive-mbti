67af3d0ccb08af154887b2a3e335f4be1bfb4853
Ahmad Naufal Abdurrohman Alfadli <ahmad.alfadli@binus.ac.id>
2026-04-26 17:21:14 +0700

    feat: add muse, aqours, liella characters

--- data/characters.js
+++ data/characters.js
@@ -1,1 +1,1 @@
-window.CHARACTERS = [...window.GROUPS.nijigasaki, ...window.GROUPS.hasunosora];
+window.CHARACTERS = [...window.GROUPS.nijigasaki, ...window.GROUPS.hasunosora, ...window.GROUPS.aqours, ...window.GROUPS.muse, ...window.GROUPS.liella];
